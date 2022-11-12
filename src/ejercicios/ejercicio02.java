398f48836f1da98f868f1c8542b480d431ebb7fd
Lydia <lydia.perez@iesnervion.es>
2022-11-12 17:50:14 +0100

    nrgjrs

--- src/ejercicios/ejercicio02.java
+++ src/ejercicios/ejercicio02.java
@@ -11,7 +11,7 @@ public class ejercicio02 {
         //la variable n donde guardaremos el numero que nos proporcione el usuario como limite
         //la variable cont para controlar las vueltas por cada numero para dividir y comprobar si es primo
         //la variable contP donde guardaremos la cantidad de primos que hay en el rango
-        int n, cont=2, contP=0;
+        int n, contP=0;
         //la variable primo para saber si un numero es primo o no
         boolean primo=true;
         //declaramos Scanner
@@ -21,15 +21,16 @@ public class ejercicio02 {
         n=sc.nextInt();
         //recorremos cada numero desde 1 hasta n comprobando si es primo o no
         //en caso de ser primo se guardara en la variable contP
-        for (int i = 1; i < n; i++) {
-            while (cont<i){
-                if (i%cont==0)
+        for (int i = n; i >1; i--) {
+            primo=true;
+            for (int j = i-1; j>=2 ; j--) {
+                if(i%j==0){
                     primo=false;
-                cont++;
-
+                    break;
+                }
             }
-            if(primo==true){
-                System.out.println(i+(" es primo"));
+            if (primo!=false){
+                System.out.println(i+" es primo.");
                 contP++;
             }
         }
