1db550f6d45ab8cd8172efe4ecbf47c4c9073d4c
lperez <lydia.perez@iesnervion.es>
2022-11-14 08:52:33 +0100

    et

--- src/ejercicios/ejercicio03.java
+++ src/ejercicios/ejercicio03.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class ejercicio03 {
 
     public static void main(String[] args) {
+        //declaro la variable donde recogeremos el valor de la base y
+        // altura que al ser un triángulo equilatero será igual
         int num;
-
+        //declaramos ek scanner
         Scanner sc = new Scanner (System.in);
-
+        //le pedimos al usuario la base y altura
         System.out.print("Introduzca una valor para el triangulo: ");
         num = sc.nextInt();
-
+        //declaramos el for que nos recorrera la altura
         for(int i = 1; i <= num; i++) {
+            //declaramos un for para imprimir los espacios y otro para imprimir el caracter
             for(int j = i; j <= num; j++) {
                 System.out.print(" ");
             }
@@ -21,7 +24,6 @@ public class ejercicio03 {
             }
             System.out.println();
         }
-        sc.close();
     }
 
 }
---
+++ src/ejercicios/ejercicio10.java
@@ -0,0 +1,8 @@
+package ejercicios;
+
+public class ejercicio10 {
+    public static void main(String[] args) {
+        int n;
+
+    }
+}
